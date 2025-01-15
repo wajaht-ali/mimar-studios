a162b96116ed011689cda201764e52aed438eff2
wajaht-ali <contact.chwajahat@gmail.com>
2025-01-15 16:34:48 +0500

    Project initialization

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react'
+import "./styles/App.css";
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs text-red-600">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
